3db6f919e074a9bb6559ae74805b57a0561e3c1b
George1044 <Georgedaou_35@hotmail.com>
2023-04-11 12:53:29 +0300

    Add speed tracker using ultrasound

---
+++ speed_tracker_arduino/speed_tracker_ultrasound/speed_tracker_ultrasound.ino
@@ -0,0 +1,89 @@
+#include <ros.h>
+#include <std_msgs/Float32.h>
+
+// PIN declarations
+#define ECHO1 2 // first ultrasound echo pin
+#define ECHO2 3 // second ultrasound echo pin
+#define PING_PIN1 50 // first ultrasound ping pin 
+#define PING_PIN2 51 // second ultrasound ping pin
+#define LED_PIN 52 // led pin, lights up when first sensor active, down when second
+#define DISTANCE 15 // disntace in cm between sensors
+
+//velocity 
+float velocity;
+
+//helpers
+long time_first;
+long time_second;
+float delta;
+bool passed = false;
+
+//rosserial setup
+ros::NodeHandle nh;
+std_msgs::Float32 velocity_msg;
+char val_str[2];
+
+ros::Publisher pub_velocity("/speed_tracker/velocity", &velocity_msg); 
+
+void  setup()
+{
+  nh.initNode();
+  nh.advertise(pub_velocity);
+  
+  pinMode(ECHO1, INPUT_PULLUP);
+  pinMode(ECHO2, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(PING_PIN1, OUTPUT);
+  pinMode(PING_PIN2, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(ECHO1), echo_1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(ECHO2), echo_2, FALLING);
+}
+
+void loop()
+{
+  int val = digitalRead(IR1);
+  String(val).toCharArray(val_str,2);
+  nh.loginfo(val_str);
+
+  // pulse the ping pins constantly
+  digitalWrite(PING_PIN1, LOW);
+  digitalWrite(PING_PIN2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(PING_PIN1, HIGH);
+  digitalWrite(PING_PIN2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PING_PIN1, LOW);
+  digitalWrite(PING_PIN2, LOW);
+  
+  nh.spinOnce();
+  delay(50);
+}
+
+void echo_1()
+{
+  if(!passed){
+    time_first = millis();
+    digitalWrite(LED_PIN, HIGH);
+    passed = true;
+    }
+}
+
+void echo_2()
+{
+  if(passed){
+    time_second = millis();
+    Serial.println("IR2 TRIGG");
+    delta = time_second - time_first;
+    velocity = (DISTANCE/delta)*10; // in m/s
+    velocity_msg.data = velocity;
+    delay(30);
+    digitalWrite(LED_PIN, LOW);
+    pub_velocity.publish(&velocity_msg);
+    passed = false;
+  }
+}
+
+
+
+
+ 
