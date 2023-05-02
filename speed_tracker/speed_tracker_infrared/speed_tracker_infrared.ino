498880981e3e8e380b1e83156d7f3b94e0211fe6
George1044 <Georgedaou_35@hotmail.com>
2023-05-02 17:54:45 +0300

    Add Sharp IR library

--- speed_tracker/speed_tracker_infrared/speed_tracker_infrared.ino
+++ speed_tracker/speed_tracker_infrared/speed_tracker_infrared.ino
@@ -1,12 +1,26 @@
 #include <ros.h>
+#include <SharpIR.h>
 #include <std_msgs/Float32.h>
 
 // PIN declarations
-#define IR1 A0 // first IR pin
-#define IR2 A1 // second IR pin
+#define IR1_pin A0 // first IR pin
+#define IR2_pin A1 // second IR pin
 #define LED_PIN 52 // led pin, lights up when first sensor active, down when second
-#define DISTANCE 8 // disntace in cm between sensors
-#define VOLTAGE_THRESHOLD 1.4 // voltage threshold that must be exceeded to consider that the IR sensor detected an object
+#define DISTANCE 10 // disntace in cm between sensors
+#define DISTANCE_THRESHOLD 30 // voltage threshold that must be exceeded to consider that the IR sensor detected an object
+#define model 1080
+
+
+/* Model :
+  GP2Y0A02YK0F --> 20150
+  GP2Y0A21YK0F --> 1080
+  GP2Y0A710K0F --> 100500
+  GP2YA41SK0F --> 430
+*/
+
+//Sharp sensor definition
+SharpIR IR1 = SharpIR(IR1_pin, model);
+SharpIR IR2 = SharpIR(IR2_pin, model);
 
 //velocity 
 float velocity;
@@ -17,13 +31,14 @@ long time_second;
 float delta;
 bool passed = false;
 bool obj_passing = false;
-float IR1_analog;
-float IR2_analog;
+float IR1_distance;
+float IR2_distance;
 
 //rosserial setup
 ros::NodeHandle nh;
 std_msgs::Float32 velocity_msg;
 char val_str[10];
+char val_str2[10];
 
 ros::Publisher pub_velocity("/speed_tracker/velocity", &velocity_msg);
 
@@ -38,27 +53,32 @@ void  setup()
 
 void loop()
 {
-  float val = analogRead(IR1) * 5.0 / 1024;
-  String(val).toCharArray(val_str,10);
+  
   //nh.loginfo(val_str);
-  IR1_analog = analogRead(IR1) * 5.0 / 1024;
-  IR2_analog = analogRead(IR2) * 5.0 / 1024;
+  IR1_distance = IR1.distance();
+  IR2_distance = IR2.distance();
+  String(IR1_distance).toCharArray(val_str,10);
+  String(IR2_distance).toCharArray(val_str2,10);
+  //nh.loginfo("IR1: ");
+  //nh.loginfo(val_str);
+  //nh.loginfo("IR2: ");
+  //nh.loginfo(val_str2);
 
-  if(IR1_analog > VOLTAGE_THRESHOLD && IR2_analog > VOLTAGE_THRESHOLD && !obj_passing){
+  if(IR1_distance < DISTANCE_THRESHOLD && IR2_distance < DISTANCE_THRESHOLD && !obj_passing){
     obj_passing = true;
     nh.loginfo("OBJ PASSING");
     digitalWrite(LED_PIN, HIGH);
       
     }
   
-  if(IR1_analog < VOLTAGE_THRESHOLD && obj_passing && !passed){
+  if(IR1_distance > DISTANCE_THRESHOLD && obj_passing && !passed){
       nh.loginfo("IR1 TRIGG");
       time_first = millis();
       digitalWrite(LED_PIN, LOW);
       passed = true;
     }
     
-  if(IR2_analog < VOLTAGE_THRESHOLD && passed && obj_passing){
+  if(IR2_distance > DISTANCE_THRESHOLD && passed && obj_passing){
       nh.loginfo("IR2 TRIGG");
       time_second = millis();
       delta = time_second - time_first;
